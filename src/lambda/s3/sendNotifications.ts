841e0bbba7e7921e5644b3279ff885d0fdda93b0
Lars Kinder <lars.kinder@porsche.de>
2021-01-04 10:19:49 +0100

    Updated sendNotification to use e.g. docClient

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,9 +1,29 @@
 import { S3Event, S3Handler } from 'aws-lambda'
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const connectionsTable = process.env.CONNECTIONS_TABLE
+const stage = process.env.STAGE
+const apiId = process.env.API_ID
+
+const connectionParams = {
+    apiVersion: "2018-11-29",
+    endpoint: `${apiId}.execute-api-eu-central-1.amazonaws.com/${stage}`
+}
 
 export const handler: S3Handler = async (event: S3Event) => {
     for (const record of event.Records) {
         const key = record.s3.object.key
         console.log('Processing S3 item with key: ', key)
+    }
+}
+
+async function sendMessageToClient(connectionId, payload) {
+    try{
+        console.log('Sending message to a connection', connectionId)
+    }catch (e){
+        console.log('Failed to send message', JSON.stringify(e))
     }
 }
