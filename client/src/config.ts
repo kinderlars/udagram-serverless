3b53560ee7af0a6a2c5e52e5c38da586e1baeffe
Lars Kinder <75133396+kinderlars@users.noreply.github.com>
2021-01-07 12:39:52 +0100

    Delete config.ts

--- client/src/config.ts
+++
@@ -1,7 +0,0 @@
-export const apiEndpoint = "https://i6hgk9nrh1.execute-api.eu-central-1.amazonaws.com/dev"
-
-export const authConfig = {
-  domain: 'dev-lb67h78z.eu.auth0.com',
-  clientId: 'anZ36oNN9H2mLaULH0p4bvtq7iAP3l6l',
-  callbackUrl: 'http://localhost:3000/callback'
-}
